a9bf5a0edfd4f63430535af1bea388099a34f2c0
Thuan Tran <thuantran190200@gmail.com>
2022-04-03 08:44:10 +0700

    update home

---
+++ src/pages/home.js
@@ -0,0 +1,191 @@
+import React from 'react';
+
+class Home extends React.Component{
+    render() {
+        return (
+            <div id='top'>
+
+            <div class="wrapper row0">
+              <header id="header" class="hoc clear"> 
+    
+                <div id="logo" class="one_quarter first">
+                  <h1><a href="index.html"><span>Y</span>15<span>Z</span>R</a></h1>
+                </div>
+                <div class="three_quarter">
+                  <ul class="nospace clear">
+                    <li class="one_third first">
+                      <div class="block clear"><a href="#"><i class="fas fa-phone"></i></a> <span><strong>Alo Admin:</strong> 0389672882</span></div>
+                    </li>
+                    <li class="one_third">
+                      <div class="block clear"><a href="#"><i class="fas fa-envelope"></i></a> <span><strong>mail:</strong> lehaidangxh@gmail.com  thuantran190200@gmail.com</span></div>
+                    </li>
+                    <li class="one_third">
+                      <div class="block clear"><a href="#"><i class="fas fa-clock"></i></a> <span><strong> Hoạt Động:</strong> 08.00am - 18.00pm</span></div>
+                    </li>
+                  </ul>
+                </div>
+              </header>
+            </div>
+
+           <div class="wrapper row1">
+            <section class="hoc clear"> 
+              <nav id="mainav">
+                <ul class="clear">
+                  <li class="active"><a href="index.html">Home</a></li>
+                  <li><a class="drop" href="#">Nhớt</a>
+                    <ul>
+                      <li><a href="pages/motul.html">Motul</a></li>
+                      <li><a href="pages/full-width.html">Resol</a></li>
+                      <li><a href="pages/sidebar-left.html">Fuchs</a></li>
+                      <li><a href="pages/sidebar-right.html">LiQui Moly</a></li>
+                      <li><a href="pages/basic-grid.html">CasTrol</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a class="drop" href="#">Phuộc Xe</a>
+                    <ul>
+                      <li><a href="#">Yss</a></li>
+                      <li><a href="#">Ohlins</a></li>
+                      <li><a href="#">RacingBoy</a></li>
+                      <li><a href="#">Nitron</a></li>
+                      
+                    </ul>
+                  </li>
+                  <li><a href="pages/mam.html">Mâm Xe</a></li>
+                  <li><a href="pages/octitan.html">Ốc Titan</a></li>
+                  <li><a href="pages/dochoi.html">Đồ Chơi Xe</a></li>
+                  <li><a href="pages/voxe.html">Vỏ Xe</a></li>
+                </ul>
+              </nav>
+      
+              <div id="searchform">
+                <div>
+                  <form action="#" method="post">
+                    <fieldset>
+                      <legend>Tìm Kiếm:</legend>
+                      <input type="text" placeholder="Enter search term&hellip;" />
+                      <button type="submit"><i class="fas fa-search"></i></button>
+                    </fieldset>
+                  </form>
+                </div>
+              </div>
+      
+            </section>
+          </div>
+
+           <div class="wrapper bgded overlay"> 
+            <div id="pageintro" class="hoc clear"> 
+              <article>
+                <p>Shop Đồ Chơi Xe Máy</p>
+                <h3 class="heading">Hải Đăng Y15ZR</h3>
+                <p>Chuyên Cung Cấp Đồ Chơi Xe Máy Chính Hãng</p>
+                <footer><a class="btn" href="#">Trãi Nghiệm Ngay <i class="fas fa-angle-right"></i></a></footer>
+              </article>
+              
+            </div>
+        </div> 
+
+          <div class="wrapper row2">
+            <section class="hoc container clear"> 
+              <div class="sectiontitle">
+                <h6 class="heading">Thông Tin Shop</h6>
+                <p>Tham Gia Và Liên Hệ Trực Tiếp Tại Đây</p>
+              </div>
+              <div id="latest" class="group">
+                <article class="one_third first"><a class="imgover" href="https://www.facebook.com/groups/310173003295730"><img src="./assets/images/demo/backgrounds/Capture.PNG" alt="" /></a>
+                  <div class="excerpt">
+                    <h6 class="heading">Group</h6>
+                    
+                    <footer><a href="#">Read More <i class="fas fa-angle-right"></i></a></footer>
+                  </div>
+                </article>
+                <article class="one_third"><a class="imgover" href="https://www.facebook.com/All-Racing-106377424321048"><img src="./assets/images/demo/backgrounds/page.PNG" alt="" /></a>
+                  <div class="excerpt">
+                    <h6 class="heading">Fanpage</h6>
+                  
+                    <footer><a href="#">Read More <i class="fas fa-angle-right"></i></a></footer>
+                  </div>
+                </article>
+                <article class="one_third"><a class="imgover" href="https://www.facebook.com/profile.php?id=100006421546739"><img src="./assets/images/demo/backgrounds/ad.PNG" alt="" /></a>
+                  <div class="excerpt">
+                    <h6 class="heading">My Facebook profile</h6>
+                  
+                    <footer><a href="#">Read More <i class="fas fa-angle-right"></i></a></footer>
+                  </div>
+                </article>
+              </div>
+              <footer class="center"><a class="btn" href="#">Tìm Hiểu <i class="fas fa-angle-right"></i></a></footer>
+            </section>
+          </div>
+
+          <div class="wrapper row4">
+            <footer id="footer" class="hoc clear"> 
+              
+              <div class="one_third first">
+                <h6 class="heading">Thông Tin Liên Hệ</h6>
+                <p>CN1: Số 5 cây cám khu phố 1 bình hưng hòa B  Bình Tân HCM.</p>
+                <p>CN2: Ấp 1, xã Vĩnh Lộc B, huyện Bình Chánh, TP.HCM </p>
+                <p class="btmspace-30">KẾT NỐI VỚI CHÚNG TÔI<a href="#"><i class="fas fa-arrow-right"></i></a></p>
+                <ul class="faico clear">
+                  <li><a class="faicon-dribble" href="#"><i class="fab fa-dribbble"></i></a></li>
+                  <li><a class="faicon-facebook" href="#"><i class="fab fa-facebook"></i></a></li>
+                  <li><a class="faicon-google-plus" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+                  <li><a class="faicon-linkedin" href="#"><i class="fab fa-linkedin"></i></a></li>
+                  <li><a class="faicon-twitter" href="#"><i class="fab fa-twitter"></i></a></li>
+                  <li><a class="faicon-vk" href="#"><i class="fab fa-vk"></i></a></li>
+                </ul>
+              </div>
+              <div class="one_third">
+                <h6 class="heading">Sản Phẩm</h6>
+                <ul class="nospace clear latestimg">
+                  <li><a href="#"><img src="./assets/images/demo/backgrounds/phuoc.jpg" alt="" /></a></li>
+                  <li><a href="#"><img src="./assets/images/demo/backgrounds/nhot.jpg" alt="" /></a></li>
+                  <li><a href="#"><img src="./assets/images/demo/backgrounds/mam.jpg" alt="" /></a></li>
+                  <li><a href="#"><img src="./assets/images/demo/backgrounds/o.jpg" alt="" /></a></li>
+                  <li><a href="#"><img src="./assets/images/demo/backgrounds/oc.jpg" alt="" /></a></li>
+                  <li><a href="pages/voxe.html"><img src="./assets/images/demo/backgrounds/vo.webp" alt="" /></a></li>
+                </ul>
+              </div>
+              <div class="one_third">
+                <h6 class="heading">Đăng Nhập Để Mua Hàng</h6>
+                <form method="post" action="#">
+                  <fieldset>
+                    <legend>Newsletter:</legend>
+                    <input class="btmspace-15" type="text" value="" placeholder="Name" />
+                    <input class="btmspace-15" type="text" value="" placeholder="Email" />
+                    
+                  <ul class="dangnhap">
+                    <li class = "dangki"> 
+                      <button type="submit" value="submit">Submit</button>
+                    
+                    </li>
+                    <li> <button type="submit" value="submit">Submit</button></li>
+                  </ul>
+                  </fieldset>
+                </form>
+              </div>
+              
+            </footer>
+          </div>
+
+          <div class="wrapper row5">
+            <div id="copyright" class="hoc clear"> 
+              
+              <p class="fl_left">Copyright &copy; 2018 - All Rights Reserved - <a href="#">Domain Name</a></p>
+              <p class="fl_right">Template by <a target="_blank" href="https://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+              
+            </div>
+          </div>
+          <div class="zalo-chat-widget" data-oaid="579745863508352884" data-welcome-message="Rất vui khi được hỗ trợ bạn!" data-autopopup="1000" data-width="350" data-height="420">dfghdsa</div>
+
+
+          <a id="backtotop" href="#top"><i class="fas fa-chevron-up"></i></a> 
+          
+
+          </div>
+        );
+    }
+}
+export default Home;
+
+
